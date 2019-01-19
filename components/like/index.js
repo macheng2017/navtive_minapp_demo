5be6daf8659fed705591b239473a2b8ad53fca1b
macheng2017 <gos360@163.com>
2019-01-19 18:43:03 +0800

    add heart icon

--- components/like/index.js
+++ components/like/index.js
@@ -3,21 +3,24 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    like: true,
+    count1: 999,
+    count2: 111,
+    count:999
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike(event) {
+      console.log(event)
+    }
   }
 })
